280da0b88a91667833465ecd1015839dff961b32
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2023-10-30 01:19:52 +0100

    Add CORS support

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 from .features.git import Git
+import os
+import json
 
 app = FastAPI()
+
+origins = json.loads(os.getenv('CORS_ORIGINS'))
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
