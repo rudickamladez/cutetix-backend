fc865d522c9b0baf9f7201a96e9337f72c0fe869
Afforix <plch.matej@gmail.com>
2023-11-07 18:59:55 +0000

    format code

--- app/main.py
+++ app/main.py
@@ -9,13 +9,13 @@ app = FastAPI(
     swagger_ui_parameters={
         "syntaxHighlight.theme": "monokai",
         "persistAuthorization": True,
-        "tryItOutEnabled": True
+        "tryItOutEnabled": True,
     },
     title="CuteTix – Cute Tickets Information System",
-    description="REST API with database of ticket reservations"
+    description="REST API with database of ticket reservations",
 )
 
-origins = json.loads(os.getenv('CORS_ORIGINS'))
+origins = json.loads(os.getenv("CORS_ORIGINS"))
 
 app.add_middleware(
     CORSMiddleware,
@@ -32,8 +32,9 @@ async def root():
     return {
         "git": git.short_hash(),
         "message": "Hello World",
-        "time": datetime.utcnow()
+        "time": datetime.utcnow(),
     }
+
 
 @app.get("/health-check")
 def health_check():
