a567d3840db62415ebd91e064ab2cef3c65d7f34
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2025-01-30 08:29:24 +0100

    Update app/main.py
    Co-authored-by: Matěj Plch <plch.matej@gmail.com>

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ import logging
 
 class EndpointFilter(logging.Filter):
     def filter(self, record: logging.LogRecord) -> bool:
-        return record.getMessage().find("/health-check") == -1
+        return "/health-check" not in record.getMessage()
 
 # Filter out route for docker container health check
 logging.getLogger("uvicorn.access").addFilter(EndpointFilter())
