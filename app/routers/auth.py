1722fd333ce8343f274d1c6a7566b4cc558ccca3
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2025-09-08 10:07:53 +0000

    fix: do not user string to identify deleting user

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -124,7 +124,7 @@ async def update_user(user: UserFromDB, db: Session = Depends(get_db)):
     description="Returns bool. Requires 'users:delete' scope.",
 )
 async def delete_user(id: UUID, db: Session = Depends(get_db)):
-    deleted = auth_service.delete(str(id), db)
+    deleted = auth_service.delete(id, db)
     if not deleted:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
