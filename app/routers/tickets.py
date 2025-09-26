9763196ad9536a0fc224d2a722d84ab77ec2a443
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2025-09-26 23:07:50 +0000

    fix: secure tickets routes by auth

--- app/routers/tickets.py
+++ app/routers/tickets.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Security
 from sqlalchemy.orm import Session
 from app import models
+from app.middleware.auth import get_current_active_user
+from app.models import TicketStatusEnum
 from app.schemas import ticket, extra
 from app.database import get_db
 from app.services import ticket as ticket_service
@@ -16,13 +18,32 @@ router = APIRouter(
 )
 
 
-@router.post("/", response_model=ticket.Ticket)
-def create_ticket(ticket: ticket.TicketPatch, db: Session = Depends(get_db)):
+@router.post(
+    "/",
+    response_model=ticket.Ticket,
+    dependencies=[Security(
+        get_current_active_user,
+        scopes=["tickets:edit"]
+    )],
+    summary="Create ticket",
+    description="Returns created object. Requires `tickets:edit` scope.",
+)
+def create_ticket(
+    ticket: ticket.TicketPatch,
+    db: Session = Depends(get_db)
+):
     return models.Ticket.create(db_session=db, **ticket.model_dump())
 
 
-@router.post("/easy", response_model=ticket.Ticket)
+@router.post(
+    "/easy",
+    response_model=ticket.Ticket,
+    summary="Create ticket easily",
+    description="Returns created object. Does not require any security scopes."
+)
 def create_ticket_easy(t: ticket.TicketCreate, db: Session = Depends(get_db)):
+    # Prevent random clients create (for examples) paid tickets
+    t.status = TicketStatusEnum.new
     t_db = create_ticket_easily(t, db)
 
     # Send error when cannot create ticket
@@ -34,7 +55,12 @@ def create_ticket_easy(t: ticket.TicketCreate, db: Session = Depends(get_db)):
     return t_db
 
 
-@router.post("/cancel", response_model=ticket.Ticket)
+@router.post(
+    "/cancel",
+    response_model=ticket.Ticket,
+    summary="Cancel ticket",
+    description="This route enable users cancel their tickets without any admin work. Does not require any security scopes."
+)
 def cancel_ticket(ct: extra.CancelTicket, db: Session = Depends(get_db)):
     t_db = read_ticket_by_id(ct.id, db)
 
@@ -44,26 +70,45 @@ def cancel_ticket(ct: extra.CancelTicket, db: Session = Depends(get_db)):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Can't cancel ticket. Ticket with given ID not found."
         )
-    ct_db: ticket.TicketPatch = ticket_service.cancel_ticket(
-        ct=ct,
-        t=t_db,
-        db=db
-    )
-    if ct_db is None:
+    try:
+        ct_db: ticket.TicketPatch = ticket_service.cancel_ticket(
+            ct=ct,
+            t=t_db,
+            db=db
+        )
+    except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Wrong e-mail or ID."
+            detail=str(e),
         )
     return ct_db
 
 
-@router.get("/", response_model=list[ticket.Ticket])
-def read_tickets(db: Session = Depends(get_db)):
+@router.get(
+    "/",
+    response_model=list[ticket.Ticket],
+    dependencies=[Security(
+        get_current_active_user,
+        scopes=["tickets:edit"]
+    )],
+    summary="Read tickets",
+    description="Returns list of object. Requires `tickets:edit` scope.",
+)
+def read_tickets(
+    db: Session = Depends(get_db)
+):
     return models.Ticket.get_all(db_session=db)
 
 
-@router.get("/{id}", response_model=ticket.Ticket)
-def read_ticket_by_id(id: int, db: Session = Depends(get_db)):
+@router.get(
+    "/{id}",
+    response_model=ticket.Ticket,
+    summary="Read ticket by ID",
+)
+def read_ticket_by_id(
+    id: int,
+    db: Session = Depends(get_db)
+):
     ticket = models.Ticket.get_by_id(db_session=db, id=id)
     if ticket is None:
         raise HTTPException(
@@ -73,17 +118,36 @@ def read_ticket_by_id(id: int, db: Session = Depends(get_db)):
     return ticket
 
 
-@router.patch(
-    "/{id}", response_model=ticket.Ticket, description="Returns updated ticket group."
+@router.put(
+    "/{id}",
+    response_model=ticket.Ticket,
+    dependencies=[Security(
+        get_current_active_user,
+        scopes=["tickets:edit"]
+    )],
+    summary="Edit ticket",
+    description="Returns updated. Requires `tickets:edit` scope.",
 )
 def update_ticket(
-    id: int, updated_ticket: ticket.TicketPatch, db: Session = Depends(get_db)
+    id: int,
+    updated_ticket: ticket.TicketPatch,
+    db: Session = Depends(get_db)
 ):
     return models.Ticket.update(db_session=db, id=id, **updated_ticket.model_dump())
 
 
 @router.delete(
-    "/{id}", response_model=ticket.Ticket, description="Returns deleted ticket group."
+    "/{id}",
+    response_model=ticket.Ticket,
+    dependencies=[Security(
+        get_current_active_user,
+        scopes=["tickets:edit"]
+    )],
+    summary="Delete ticket",
+    description="Returns deleted ticket. Requires `tickets:edit` scope.",
 )
-def delete_ticket(id: int, db: Session = Depends(get_db)):
+def delete_ticket(
+    id: int,
+    db: Session = Depends(get_db)
+):
     return models.Ticket.delete(db_session=db, id=id)
